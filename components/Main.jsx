0f9b46a99acedf8cac0459633240223b0ff43e4d
ts4blader <ts4blader@gmail.com>
2021-10-06 22:21:07 +0700

    set-up a paginate

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "/components/Card";
 import Aside from "/components/Aside";
 
+const ITEM_PER_PAGE = 6;
+const DEFAULT_PAGE = 1;
+
 export default function Main({ posts, allPosts }) {
+  const [page, setPage] = useState(DEFAULT_PAGE);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    let start = (page - 1) * 6;
+    let end = page * 6;
+
+    setData([...posts].slice(start, end));
+  }, [page, posts]);
+
   return (
     <main className="main">
       <div className="container content">
         <ul className="cards">
-          {posts.map((item) => (
+          {data.map((item) => (
             <li key={item.fields.slug}>
               <Card post={item} />
             </li>
@@ -18,7 +31,32 @@ export default function Main({ posts, allPosts }) {
         {/* End Aside */}
       </div>
       <div className="container">
-        <div className="paginate"></div>
+        <div className="paginate">
+          <div className="prev" data-disabled={page === 1}>
+            prev
+          </div>
+          <ul>
+            {[...Array(parseInt(posts.length / ITEM_PER_PAGE + 1)).keys()].map(
+              (item) => {
+                return (
+                  <li
+                    key={item}
+                    data-active={item + 1 === page}
+                    onClick={() => setPage(item + 1)}
+                  >
+                    {item + 1}
+                  </li>
+                );
+              }
+            )}
+          </ul>
+          <div
+            className="next"
+            data-disabled={page === parseInt(posts.length / ITEM_PER_PAGE) + 1}
+          >
+            next
+          </div>
+        </div>
       </div>
     </main>
   );
