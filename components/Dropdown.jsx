891cb64084e4264f63017155757020ad29c16476
ts4blader <ts4blader@gmail.com>
2021-10-07 18:10:21 +0700

    dropdown adjust

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import Icon from "/components/Icon";
 import Link from "next/link";
 
@@ -6,22 +6,21 @@ export default function Dropdown({ agent, items }) {
   const [show, setShow] = useState(false);
 
   return (
-    <div className="dropdown" data-show={show}>
-      <div className="dropdown__agent" onClick={() => setShow(false)}>
+    <div
+      className="dropdown"
+      data-show={show}
+      onMouseLeave={() => setShow(false)}
+    >
+      <div className="dropdown__agent" onClick={() => setShow((prev) => !prev)}>
         <p>{agent}</p>
         <Icon src="dropdown.png" alt="caret" />
-        <input
-          type="text"
-          onFocus={() => setShow(true)}
-          onBlur={() => setShow(false)}
-        />
       </div>
       <ul className="dropdown__items">
         <li>
           <Link href="/">all</Link>
         </li>
         {items.map((item, index) => (
-          <li key={index}>
+          <li key={index} onClick={() => setShow(false)}>
             <Link href={item.link}>{item.name}</Link>
           </li>
         ))}
